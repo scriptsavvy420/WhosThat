ac459db46f8a8d9bfb0006afc3aeb211e8345e4d
Script Savvy <scriptsavvy0420@gmail.com>
2022-07-28 12:49:56 +0530

    now feth and showing pokemon done

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,10 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Game.css";
 import WTPLogoImg from "../images/whosthatpokemon.png";
 import PikachuTestImg from "../images/pikachu.png";
 
 const Game = () => {
-  // Todo so some loading screen of WTPLogoImg
+  const totalPokemons = 905;
+
+  const [loadNewPokemon, setLoadNewPokemon] = useState(false);
+  const [currentPokemon, setCurrentPokemon] = useState({});
+  const [answerOptions, setAnswerOptions] = useState([]);
+
+  const getRandomPokemonId = () => {
+    return Math.floor(Math.random() * (totalPokemons + 1));
+  };
+
+  useEffect(() => {
+    const randomPokemonFetchUrl = `https://pokeapi.co/api/v2/pokemon/${getRandomPokemonId()}`;
+    fetch(randomPokemonFetchUrl)
+      .then((response) => response.json())
+      .then((pokemonData) => {
+        console.log(pokemonData);
+        fetch(
+          `https://pokeapi.co/api/v2/pokemon?limit=10&offset=${getRandomPokemonId()}`
+        )
+          .then((resp) => resp.json())
+          .then((data) => {
+            const options = getOptionsAndSuffle(
+              data?.results,
+              pokemonData?.name
+            );
+            setAnswerOptions(options);
+            setCurrentPokemon(pokemonData);
+          });
+      });
+  }, [loadNewPokemon]);
 
   return (
     <>
@@ -12,7 +41,7 @@ const Game = () => {
         <div className="blurred_shadow"></div>
         <div className="navbar">
           <h1 className="logo">Who's That</h1>
-          <h2 className="high__score">High Score - 392</h2>
+          <h2 className="high__score">High Score - 0</h2>
         </div>
       </div>
 
@@ -24,24 +53,28 @@ const Game = () => {
             className="wtp__logo"
           />
           <img
-            src={PikachuTestImg}
+            src={currentPokemon?.sprites?.other?.home?.front_default}
             alt="Pokemon Shadow!"
+            onDragStart={(e) => {
+              e.preventDefault();
+            }}
             className="pokemon__img"
           />
         </div>
         <div className="whos_pokemon_details">
           <div className="score__time_container">
-            <div className="yellow__box">Score - 0</div>
+            <div className="yellow__box">Score - 20</div>
             <div className="yellow__box">Time - 30</div>
             <div className="yellow__box">Home</div>
             <div className="yellow__box">Restart</div>
           </div>
 
           <div className="option__container">
-            <div className="box__unfill">Bulbasaur</div>
-            <div className="box__unfill">Charizard</div>
-            <div className="box__unfill">Blostoise</div>
-            <div className="box__unfill">Genesect</div>
+            {answerOptions?.map((pokemon, index) => (
+              <div className="box__unfill" key={index + pokemon.name}>
+                {pokemon?.name}
+              </div>
+            ))}
           </div>
 
           <div className="game__right-shadow">
@@ -53,5 +86,34 @@ const Game = () => {
     </>
   );
 };
+
+function getOptionsAndSuffle(results, currentPokemonName) {
+  const options = [];
+  options.push({ name: currentPokemonName, url: null });
+  options.push(results[0]);
+  options.push(results[3]);
+  options.push(results[6]);
+  return shuffle(options);
+}
+
+function shuffle(array) {
+  let currentIndex = array.length,
+    randomIndex;
+
+  // While there remain elements to shuffle.
+  while (currentIndex != 0) {
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
+
+  return array;
+}
 
 export default Game;
